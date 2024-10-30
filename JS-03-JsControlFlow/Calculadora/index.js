9579e6a4c139e7ce6b21a5499b8af8d77089425c
Hampsli <aguirre.wl@gmail.com>
2024-10-30 14:39:12 -0600

    adding new functions to our calculator

--- JS-03-JsControlFlow/Calculadora/index.js
+++ JS-03-JsControlFlow/Calculadora/index.js
@@ -1,10 +1,26 @@
-let numero2 = parseFloat(document.getElementById("numero2").value);
 let resultado; 
 
-function onInputChange(){
+function onInputOneChange(){
     let numero1 = parseFloat(document.getElementById("numero1").value);
-    console.log(numero1);
+    let numero2 = parseFloat(document.getElementById("numero2").value);
+   
 }
 
+function operacionMatematica(typoOperacion){
+    switch(typoOperacion){
+        case "suma":
+            resultado = numero1 + numero2;
+            break;
+        case "resta":
+            resultado = numero1 - numero2;
+            break;
+        case "multi":
+            resultado = numero1 * numero2;
+            break;
+        default:
+             alert("syntax error");
+    }
+    console.log(resultado);
+}
 
 
