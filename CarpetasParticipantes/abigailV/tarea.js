a88caf344d27382102e8828c1e8148fe56e11d73
abivilleda28 <abigailfloresvilleda@gmail.com>
2024-10-30 00:10:21 -0600

    agregando tarea

--- CarpetasParticipantes/abigailV/tarea.js
+++ CarpetasParticipantes/abigailV/tarea.js
@@ -10,27 +10,20 @@
 // Maria has to calculate the cost of her payments for the month. For every transation there is a $3 fee and a 0.1% (0.01) interest fee. Can you help her calculate her costs?
 // Return the value of what she should be paying.
 //función 
-function calculoTotal{
-    const transation = 0.001;
-    const fee = 3;
-    let transacciones = 
-    let 
-    return totalTransaccion;
-}
-console.log(costoTotal)
+
 
 // Exercise #2
 // Part 1
 // Ed would like a way to input 3 names of his friends. The output should be a console greeting to his friends saying: Welcome {name1}, {name2}, {name3}.
-let nombre = "Jesus";
+let nombre = prompt ("Ingresa tu nombre: ");
 
-let nombre1= "María";
+let nombre1= prompt ("Ingresa tu nombre: ");
 
-let nombre3 = "Juan;
+let nombre2 = prompt ("Ingresa tu nombre:");
 
-console.log{
-    ("Welcome", nombre,nombre1,nombre3)
-}
+let saludo = "Welcome"+ " " + nombre + " " +nombre1+ " " + nombre2 ; 
+
+console.log(saludo)
 
 // Part 2
 // Ed would like to create a function that takes in a birth year and returns the age.
@@ -39,15 +32,69 @@ console.log{
 
 // Part 3
 // Ed would like to create a function that prints out, Welcome {name1}, you are {age1}. Welcome {name2}, you are {age2}. Welcome {name3}, you are {age3}.
+let nombre3 = prompt ("Ingresa tu nombre: ");
+
+let edad= prompt ("Ingresa tu edad: ");
+
+let saludo1("Welcome" + nombre +"tienes" + edad +"años");
+
+console.log(saludo1)
+
+let nombre4 = prompt ("Ingresa tu nombre:");
+
+let edad2= prompt ("Ingresa tu edad: ");
+
+console.log("Welcome" + nombre2 + edad2);
+
+let nombre5 = prompt ("Ingresa tu nombre:");
+
+let edad3= prompt ("Ingresa tu edad: ");
+
+console.log("Welcome" + nombre3 + edad3);
+
+
+
+
 
 // Challenge Yourself
 // A teacher wants to create a rubric for grading. The rubric is from 0 - 11.
 
 // Part 1
 // A student passes if they have a score greater than or equal to 5. Create a function that returns a boolean true or false.
+function aprobar(calificacion){
+return calificacion >= 5;
+}
+
+let calificacionEstudiante = 5;
+let calificacionEstudiante1 = 4;
+
+console.log(aprobar(calificacionEstudiante));
+console.log(aprobar(calificacionEstudiante1));
 
 // Part 2
 // A student has an excellent grade if they score higher than 8. Add on to your function to print out "Excellent" for scores greater than 8.
+function excelente(calificacion){
+    if (calificacion >= 8) {
+        console.log("Excelente");
+    }
+}
+
+let calificacionExcelente = 8;
+let calificacionExcelente1 = 7;
+
+console.log(excelente(calificacionExcelente));
+
+
 
 // Part 3
 // A student has a perfect grade if their score is 11. Add on to your function to print out "Perfect" for a score of 11.
+function perfecto(calificacion){
+    if (calificacion >= 11) {
+        console.log("Perfecto");
+    }
+}
+
+let calificacionPerfecto = 11;
+
+
+console.log(perfecto(calificacionPerfecto));
