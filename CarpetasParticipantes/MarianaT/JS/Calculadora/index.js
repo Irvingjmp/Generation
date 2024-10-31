ea0cfd8927edcd51758bb9bf92065a4d6363e3ec
Mariana Torres <marith.3289@gmail.com>
2024-10-30 20:06:57 -0600

    calculadora actualizada con impresiones en consola

--- CarpetasParticipantes/MarianaT/JS/Calculadora/index.js
+++ CarpetasParticipantes/MarianaT/JS/Calculadora/index.js
@@ -1,17 +1,19 @@
-// Calculadora básica
 function append(value) {
-    document.getElementById("display").value += value;
+  document.getElementById("display").value += value;
+  console.log(document.getElementById("display").value);
 }
 
 function clearDisplay() {
-    document.getElementById("display").value = '';
+  document.getElementById("display").value = "";
 }
 
 function calcular() {
-    try {
-        let resultado = eval(document.getElementById("display").value);
-        document.getElementById("display").value = resultado;
-    } catch (error) {
-        document.getElementById("display").value = 'Error';
-    }
+  try {
+    let resultado = eval(document.getElementById("display").value);
+    document.getElementById("display").value = resultado;
+    console.log(document.getElementById("display").value);
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+    console.log(document.getElementById("display").value);
+  }
 }
